4b5dc012b0de283d65b90c46f35093c4b4373497
Alireza <alirezagoldoos@gmail.com>
2024-01-03 22:39:52 +0330

    Add: add adsadd view

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Ads
+from django.http import HttpResponse
+from .models import Ads, Car, Brand, Seller
+import datetime
 
 # Create your views here.
 def ads_list(request):
@@ -14,4 +16,45 @@ def ads_list(request):
         adss = adss.filter(price__gte=selected_min)
     if selected_max != None:
         adss = adss.filter(price__lte=selected_max)
-    return render(request, 'adspage/ads.html', {'adss':adss})
+    return render(request, 'adspage/ads.html', {'adss':adss.order_by('-nardebam')})
+
+def add_ads(request):
+    if request.method == 'GET':
+        return render(request, 'adspage/addads.html', {'cars':Car.objects.all(), 'brands':Brand.objects.all()})
+    else:
+        # print(request.POST.getlist())
+        name = request.POST['name']
+        user_type = request.POST['selected_type']
+        selected_car = request.POST['selected_car']
+        selected_brand = request.POST['selected_brand']
+        color = request.POST['color']
+        city = request.POST['city']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        kilometer = request.POST['kilometer']
+        price = request.POST['price']
+        year = request.POST['year']
+        try:
+            if request.POST['nardebam']:
+                nardebam = True
+        except:
+            nardebam = False
+        description = request.POST['description']
+        try:
+            seller = Seller.objects.get(name=name)
+        except:
+            seller = Seller.objects.create(name=name, phone_number=phone_number, verified=False, type=user_type)
+        Ads.objects.create(
+            car=Car.objects.get(name=selected_car),
+            seller=seller,
+            city=city,
+            address=address,
+            price=price,
+            description=description,
+            kilometer=kilometer,
+            year=year,
+            color=color,
+            posting_time=datetime.datetime.today(),
+            nardebam=nardebam,
+        )
+        return render(request, 'adspage/added.html', context={'Success':True})
