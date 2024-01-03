8009d819d1c5eba006526410edc997615016b2ab
Alireza <alirezagoldoos@gmail.com>
2024-01-03 22:38:29 +0330

    Update: rename location fieldt to address

--- ads/models.py
+++ ads/models.py
@@ -26,6 +26,7 @@ class Seller(models.Model):
     type = models.CharField(max_length=50, choices=(('shakhsi', 'shakhsi'),
                                                     ('bongah', 'bongah'),
                                                     ('namayeshgah','namayeshgah')), default='shakhsi')
+    phone_number=models.CharField(max_length=11)
     verified = models.BooleanField()
     def __str__(self) -> str:
         return self.name
@@ -34,10 +35,11 @@ class Ads(models.Model):
     car = models.ForeignKey(Car, on_delete=models.PROTECT)
     seller = models.ForeignKey(Seller, on_delete=models.PROTECT)
     city = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     description = models.TextField()
     kilometer = models.IntegerField()
     year = models.CharField(max_length=4)
     color = models.CharField(max_length=20)
     posting_time = models.DateTimeField(editable=False, default=datetime.datetime.now())
+    nardebam = models.BooleanField()
